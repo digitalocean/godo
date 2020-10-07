22527dc3b8dd7e9afb86afb6efc4d183b507f05b
Ivan Lemeshev <vanlmshv@gmail.com>
2020-10-07 22:52:39 +0300

    Add expiry_seconds assert in test (#334)

--- kubernetes_test.go
+++ kubernetes_test.go
@@ -420,6 +420,10 @@ func TestKubernetesClusters_GetKubeConfigWithExpiry(t *testing.T) {
 	blob := []byte(want)
 	mux.HandleFunc("/v2/kubernetes/clusters/deadbeef-dead-4aa5-beef-deadbeef347d/kubeconfig", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, http.MethodGet)
+		expirySeconds, ok := r.URL.Query()["expiry_seconds"]
+		assert.True(t, ok)
+		assert.Len(t, expirySeconds, 1)
+		assert.Contains(t, expirySeconds, "3600")
 		fmt.Fprint(w, want)
 	})
 	got, _, err := kubeSvc.GetKubeConfigWithExpiry(ctx, "deadbeef-dead-4aa5-beef-deadbeef347d", 3600)
