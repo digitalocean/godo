11346c6a5978623e3070241a9f635a2513cd55a9
Braxton Huggins <bhuggins@digitalocean.com>
2021-08-05 13:36:07 -0700

    Prep v1.65.0 release

--- godo.go
+++ godo.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.64.2"
+	libraryVersion = "1.65.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
