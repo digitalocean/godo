db10ddb1ee9c7b1929c52af3d05f15028bacc8c4
Chris Cummer <chriscummer@me.com>
2022-04-12 14:06:08 -0700

    Remove DisablePublicNetworking option from the Create path (#528)
    Signed-off-by: Chris Cummer <chriscummer@me.com>

--- godo_test.go
+++ godo_test.go
@@ -276,34 +276,6 @@ func TestNewRequest_withDropletAgent(t *testing.T) {
 	}
 }
 
-func TestNewRequest_DisablePublicNetworking(t *testing.T) {
-	c := NewClient(nil)
-
-	inURL, outURL := "/foo", defaultBaseURL+"foo"
-	inBody, outBody := &DropletCreateRequest{Name: "l", DisablePublicNetworking: true},
-		`{"name":"l","region":"","size":"","image":0,`+
-			`"ssh_keys":null,"backups":false,"ipv6":false,`+
-			`"private_networking":false,"monitoring":false,"tags":null,"disable_public_networking":true}`+"\n"
-	req, _ := c.NewRequest(ctx, http.MethodPost, inURL, inBody)
-
-	// test relative URL was expanded
-	if req.URL.String() != outURL {
-		t.Errorf("NewRequest(%v) URL = %v, expected %v", inURL, req.URL, outURL)
-	}
-
-	// test body was JSON encoded
-	body, _ := ioutil.ReadAll(req.Body)
-	if string(body) != outBody {
-		t.Errorf("NewRequest(%v)Body = %v, expected %v", inBody, string(body), outBody)
-	}
-
-	// test default user-agent is attached to the request
-	userAgent := req.Header.Get("User-Agent")
-	if c.UserAgent != userAgent {
-		t.Errorf("NewRequest() User-Agent = %v, expected %v", userAgent, c.UserAgent)
-	}
-}
-
 func TestNewRequest_badURL(t *testing.T) {
 	c := NewClient(nil)
 	_, err := c.NewRequest(ctx, http.MethodGet, ":", nil)
