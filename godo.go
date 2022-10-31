e1e5f0f28a7fa0a8b538c09efde1b782fc44f023
Andrew Starr-Bochicchio <andrewsomething@users.noreply.github.com>
2022-10-31 18:16:15 -0400

    Prep v1.88.0 release. (#574)
    * Prep v1.88.0 release.
    
    * Bump date.

--- godo.go
+++ godo.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.87.0"
+	libraryVersion = "1.88.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
