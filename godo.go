470fbee3b3aebfabb8fc7832832761e993bad924
Aditya Saha <asaha@digitalocean.com>
2023-07-20 13:24:51 -0400

    Prepare to release v2.0.0

--- godo.go
+++ godo.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	libraryVersion = "1.99.0"
+	libraryVersion = "2.0.0"
 	defaultBaseURL = "https://api.digitalocean.com/"
 	userAgent      = "godo/" + libraryVersion
 	mediaType      = "application/json"
