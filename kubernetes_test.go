b2d8fdfe8d917d74b6f3bcea4c186421d59793f9
llDrLove <oliver.love.34@gmail.com>
2024-06-14 11:25:45 -0400

    Rename control plane permission to control plane firewall (#701)
    * CON-10347 rename control plane permission to control plane firewall
    
    * CON-10347
    
    ---------
    
    Co-authored-by: Oliver Love <olove@digitalocean.com>

--- kubernetes_test.go
+++ kubernetes_test.go
@@ -569,7 +569,7 @@ func TestKubernetesClusters_Create(t *testing.T) {
 			StartTime: "00:00",
 			Day:       KubernetesMaintenanceDayMonday,
 		},
-		ControlPlanePermission: &KubernetesControlPlanePermission{
+		ControlPlaneFirewall: &KubernetesControlPlaneFirewall{
 			Enabled: &enabled,
 			AllowedAddresses: []string{
 				"1.2.3.4/32",
@@ -633,7 +633,7 @@ func TestKubernetesClusters_Create(t *testing.T) {
 			"start_time": "00:00",
 			"day": "monday"
 		},
-        "control_plane_permission": {
+        "control_plane_firewall": {
              "enabled": true,
              "allowed_addresses": [
                  "1.2.3.4/32"
@@ -797,7 +797,7 @@ func TestKubernetesClusters_Update(t *testing.T) {
 			StartTime: "00:00",
 			Day:       KubernetesMaintenanceDayMonday,
 		},
-		ControlPlanePermission: &KubernetesControlPlanePermission{
+		ControlPlaneFirewall: &KubernetesControlPlaneFirewall{
 			Enabled: &enabled,
 			AllowedAddresses: []string{
 				"1.2.3.4/32",
@@ -809,7 +809,7 @@ func TestKubernetesClusters_Update(t *testing.T) {
 		Tags:              want.Tags,
 		MaintenancePolicy: want.MaintenancePolicy,
 		SurgeUpgrade:      true,
-		ControlPlanePermission: &KubernetesControlPlanePermission{
+		ControlPlaneFirewall: &KubernetesControlPlaneFirewall{
 			Enabled: &enabled,
 			AllowedAddresses: []string{
 				"1.2.3.4/32",
@@ -851,7 +851,7 @@ func TestKubernetesClusters_Update(t *testing.T) {
 			"start_time": "00:00",
 			"day": "monday"
 		},
-		"control_plane_permission": {
+		"control_plane_firewall": {
              "enabled": true,
              "allowed_addresses": [
                  "1.2.3.4/32"
@@ -860,7 +860,7 @@ func TestKubernetesClusters_Update(t *testing.T) {
 	}
 }`
 
-	expectedReqJSON := `{"name":"antoine-test-cluster","tags":["cluster-tag-1","cluster-tag-2"],"maintenance_policy":{"start_time":"00:00","duration":"","day":"monday"},"surge_upgrade":true,"control_plane_permission":{"enabled":true,"allowed_addresses":["1.2.3.4/32"]}}
+	expectedReqJSON := `{"name":"antoine-test-cluster","tags":["cluster-tag-1","cluster-tag-2"],"maintenance_policy":{"start_time":"00:00","duration":"","day":"monday"},"surge_upgrade":true,"control_plane_firewall":{"enabled":true,"allowed_addresses":["1.2.3.4/32"]}}
 `
 
 	mux.HandleFunc("/v2/kubernetes/clusters/8d91899c-0739-4a1a-acc5-deadbeefbb8f", func(w http.ResponseWriter, r *http.Request) {
