4ac4be29641ae97d5f752d4c66b223830b8162dd
Alberto Pinon Formoso <apinonformoso@digitalocean.com>
2025-03-18 10:11:27 -0600

    VPC-4312: lint

--- partner_network_connect.go
+++ partner_network_connect.go
@@ -200,7 +200,7 @@ type partnerNetworkConnectAttachmentRoot struct {
 func (r *partnerNetworkConnectAttachmentRoot) UnmarshalJSON(data []byte) error {
 	// auxiliary structure to capture both potential keys
 	var aux struct {
-		PartnerNetworkConnect                *PartnerNetworkConnect `json:"partner_network_connect"`
+		PartnerNetworkConnect         *PartnerNetworkConnect `json:"partner_network_connect"`
 		PartnerInterconnectAttachment *PartnerNetworkConnect `json:"partner_interconnect_attachment"`
 	}
 	if err := json.Unmarshal(data, &aux); err != nil {
@@ -217,16 +217,16 @@ func (r *partnerNetworkConnectAttachmentRoot) UnmarshalJSON(data []byte) error {
 
 type partnerNetworkConnectsRoot struct {
 	PartnerNetworkConnects []*PartnerNetworkConnect `json:"-"`
-	Links           *Links            `json:"links"`
-	Meta            *Meta             `json:"meta"`
+	Links                  *Links                   `json:"links"`
+	Meta                   *Meta                    `json:"meta"`
 }
 
 func (r *partnerNetworkConnectsRoot) UnmarshalJSON(data []byte) error {
 	var aux struct {
 		PartnerInterconnectAttachments []*PartnerNetworkConnect `json:"partner_interconnect_attachments"`
-		PartnerNetworkConnects                []*PartnerNetworkConnect `json:"partner_network_connects"`
-		Links                          *Links            `json:"links"`
-		Meta                           *Meta             `json:"meta"`
+		PartnerNetworkConnects         []*PartnerNetworkConnect `json:"partner_network_connects"`
+		Links                          *Links                   `json:"links"`
+		Meta                           *Meta                    `json:"meta"`
 	}
 
 	if err := json.Unmarshal(data, &aux); err != nil {
