0a49832c4c2b2bb70320f842140f3904709b3507
v-amanjain <v-amanjain@digitalocean.com>
2026-02-04 19:25:50 +0530

    switch performance tier of nfs share

--- nfs_actions_test.go
+++ nfs_actions_test.go
@@ -88,3 +88,23 @@ func TestNfsDetach(t *testing.T) {
 	assert.Equal(t, "in-progress", action.Status)
 	assert.Equal(t, "detach", action.Type)
 }
+
+func TestNfsSwitchPerformanceTier(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/nfs/my-nfs-id/actions", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprint(w, `{"action": {"id": 1, "status": "in-progress", "type": "switch_performance_tier", "resource_type": "network_file_share", "resource_id": "my-nfs-id", "region_slug": "atl1", "started_at": "2025-10-14T11:55:31.615157397Z"}}`)
+	})
+
+	action, resp, err := client.NfsActions.SwitchPerformanceTier(context.Background(), "my-nfs-id", "standard")
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, "my-nfs-id", action.ResourceID)
+	assert.Equal(t, "atl1", action.RegionSlug)
+	assert.Equal(t, "network_file_share", action.ResourceType)
+	assert.Equal(t, "in-progress", action.Status)
+	assert.Equal(t, "switch_performance_tier", action.Type)
+}
