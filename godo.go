7f37a21132dd4dbedf936e90ede483972cbd308c
Ben Tranter <ben@bentranter.io>
2026-02-10 10:57:26 -0500

    security: add cspm scanning functionality for public preview
    Implements the API endpoints for the upcoming CSPM product. This enters
    public preview later this year, so this will only work for users enabled
    for this feature for now.

--- godo.go
+++ godo.go
@@ -78,6 +78,7 @@ type Client struct {
 	Kubernetes          KubernetesService
 	LoadBalancers       LoadBalancersService
 	Monitoring          MonitoringService
+	Security            SecurityService
 	Nfs                 NfsService
 	NfsActions          NfsActionsService
 	OneClick            OneClickService
@@ -306,6 +307,7 @@ func NewClient(httpClient *http.Client) *Client {
 	c.Kubernetes = &KubernetesServiceOp{client: c}
 	c.LoadBalancers = &LoadBalancersServiceOp{client: c}
 	c.Monitoring = &MonitoringServiceOp{client: c}
+	c.Security = &SecurityServiceOp{client: c}
 	c.Nfs = &NfsServiceOp{client: c}
 	c.NfsActions = &NfsActionsServiceOp{client: c}
 	c.VPCNATGateways = &VPCNATGatewaysServiceOp{client: c}
