186005a6da71e047da4716f0d325ee55f651802a
Anit Gandhi <agandhi@digitalocean.com>
2026-03-27 17:11:14 -0500

    droplets: add optional public_networking bool to create request

--- godo_test.go
+++ godo_test.go
@@ -284,6 +284,92 @@ func TestNewRequest_withDropletAgent(t *testing.T) {
 	}
 }
 
+func TestNewRequest_PublicNetworking_NotPresent(t *testing.T) {
+	c := NewClient(nil)
+
+	inURL, outURL := "/foo", defaultBaseURL+"foo"
+	inBody := &DropletCreateRequest{Name: "l"}
+	outBody := `{"name":"l","region":"","size":"","image":0,` +
+		`"ssh_keys":null,"backups":false,"ipv6":false,` +
+		`"private_networking":false,"monitoring":false,"tags":null}` + "\n"
+	req, _ := c.NewRequest(ctx, http.MethodPost, inURL, inBody)
+
+	// test relative URL was expanded
+	if req.URL.String() != outURL {
+		t.Errorf("NewRequest(%v) URL = %v, expected %v", inURL, req.URL, outURL)
+	}
+
+	// test body was JSON encoded
+	body, _ := io.ReadAll(req.Body)
+	if string(body) != outBody {
+		t.Errorf("NewRequest(%v)Body = %v, expected %v", inBody, string(body), outBody)
+	}
+
+	// test default user-agent is attached to the request
+	userAgent := req.Header.Get("User-Agent")
+	if c.UserAgent != userAgent {
+		t.Errorf("NewRequest() User-Agent = %v, expected %v", userAgent, c.UserAgent)
+	}
+}
+
+func TestNewRequest_PublicNetworking_True(t *testing.T) {
+	c := NewClient(nil)
+
+	trueVal := true
+	inURL, outURL := "/foo", defaultBaseURL+"foo"
+	inBody := &DropletCreateRequest{Name: "l", PublicNetworking: &trueVal}
+	outBody := `{"name":"l","region":"","size":"","image":0,` +
+		`"ssh_keys":null,"backups":false,"ipv6":false,` +
+		`"private_networking":false,"monitoring":false,"tags":null,"public_networking":true}` + "\n"
+	req, _ := c.NewRequest(ctx, http.MethodPost, inURL, inBody)
+
+	// test relative URL was expanded
+	if req.URL.String() != outURL {
+		t.Errorf("NewRequest(%v) URL = %v, expected %v", inURL, req.URL, outURL)
+	}
+
+	// test body was JSON encoded
+	body, _ := io.ReadAll(req.Body)
+	if string(body) != outBody {
+		t.Errorf("NewRequest(%v)Body = %v, expected %v", inBody, string(body), outBody)
+	}
+
+	// test default user-agent is attached to the request
+	userAgent := req.Header.Get("User-Agent")
+	if c.UserAgent != userAgent {
+		t.Errorf("NewRequest() User-Agent = %v, expected %v", userAgent, c.UserAgent)
+	}
+}
+
+func TestNewRequest_PublicNetworking_False(t *testing.T) {
+	c := NewClient(nil)
+
+	falseVal := false
+	inURL, outURL := "/foo", defaultBaseURL+"foo"
+	inBody := &DropletCreateRequest{Name: "l", PublicNetworking: &falseVal}
+	outBody := `{"name":"l","region":"","size":"","image":0,` +
+		`"ssh_keys":null,"backups":false,"ipv6":false,` +
+		`"private_networking":false,"monitoring":false,"tags":null,"public_networking":false}` + "\n"
+	req, _ := c.NewRequest(ctx, http.MethodPost, inURL, inBody)
+
+	// test relative URL was expanded
+	if req.URL.String() != outURL {
+		t.Errorf("NewRequest(%v) URL = %v, expected %v", inURL, req.URL, outURL)
+	}
+
+	// test body was JSON encoded
+	body, _ := io.ReadAll(req.Body)
+	if string(body) != outBody {
+		t.Errorf("NewRequest(%v)Body = %v, expected %v", inBody, string(body), outBody)
+	}
+
+	// test default user-agent is attached to the request
+	userAgent := req.Header.Get("User-Agent")
+	if c.UserAgent != userAgent {
+		t.Errorf("NewRequest() User-Agent = %v, expected %v", userAgent, c.UserAgent)
+	}
+}
+
 func TestNewRequest_badURL(t *testing.T) {
 	c := NewClient(nil)
 	_, err := c.NewRequest(ctx, http.MethodGet, ":", nil)
